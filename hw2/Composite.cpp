b25800f8c74405fe66cb59dbe829affd30fcc8e9
Ayah Harper <harperhp53@gmail.com>
2024-10-05 21:00:34 -0400

    composite draw and draw normal

--- hw2/Composite.cpp
+++ hw2/Composite.cpp
@@ -20,18 +20,43 @@ Composite::~Composite() {}
 
 /* draw
 *
-* 
+* applies the composite shapes'
+* transform matrix before 
+* drawing all sub-shapes
 *
 */
 void Composite::draw() {
-    
+
+    glPushMatrix();
+
+    // apply the current composite transform 
+    glMultMatrixf(glm::value_ptr(transMatrix))
+
+    // draw all the shapes
+    (for int i = 0; i < shapes.size(); i++) {
+        shapes[i].draw();
+    }
+    glPopMatrix();
+
 }
 
 /* drawNormal
 *
-* 
+* applies the composite shapes'
+* transform matrix before 
+* drawing the normals of all sub-shapes
 *
 */
 void Composite::drawNormal() {
-    
+
+    glPushMatrix();
+
+    // apply the current composite transform 
+    glMultMatrixf(glm::value_ptr(transMatrix))
+
+    // draw all the shapes' normals
+    (for int i = 0; i < shapes.size(); i++) {
+        shapes[i].drawNormal();
+    }
+    glPopMatrix();
 }
