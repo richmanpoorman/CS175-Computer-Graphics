c53f864afc8c209ed0d0e39a714fb3eb43550aac
Snw125 <123609058+Snw125@users.noreply.github.com>
2024-10-05 20:23:32 -0400

    composite class

---
+++ hw2/Composite.cpp
@@ -0,0 +1,37 @@
+#include "Composite.h"
+#include "Shape.h"
+#include <iostream>
+
+using namespace std;
+
+/* constructor
+*
+* 
+*/
+Composite::Composite() {
+
+}
+
+/* destructor
+*/
+Composite::~Composite() {}
+
+
+
+/* draw
+*
+* 
+*
+*/
+void Composite::draw() {
+    
+}
+
+/* drawNormal
+*
+* 
+*
+*/
+void Composite::drawNormal() {
+    
+}
