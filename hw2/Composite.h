4f00ec5ac09019eb591e611f1710eb494c78cddc
Snw125 <123609058+Snw125@users.noreply.github.com>
2024-10-05 20:28:33 -0400

    composite update

--- hw2/Composite.h
+++ hw2/Composite.h
@@ -1,1 +1,26 @@
-#pragma once
+#ifndef COMPOSITE_H
+#define COMPOSITE_H
+
+#include "Shape.h"
+#include "Geometry.h"
+
+class Composite : public Shape {
+public:
+	Composite();
+	~Composite();
+
+	OBJ_TYPE getType() {
+		return SHAPE_SCENE;
+	}
+
+
+	void draw();
+	void drawNormal();
+
+
+private:
+	vector<Surface> shapes;
+	glm::mat4 transMatrix;
+};
+
+#endif
